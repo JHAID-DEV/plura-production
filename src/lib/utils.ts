d8f44119157ff1817853b630c80cf362f7eeb35a
PRIGID BUSINESS LIMITED <kpormegbegideonsuccess@gmail.com>
2025-01-27 04:35:39 +0000

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getStripeOAuthLink(
   accountType: 'agency' | 'subaccount',
-  state: string
+  state: string,
+  clientId: string,
+  redirectUri: string
 ) {
-  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`
+  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${clientId}&scope=read_write&redirect_uri=${redirectUri}${accountType}&state=${state}`
 }
